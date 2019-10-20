c335cec38c5fc90d3fc4ee9320d014a1020ecfcf
ikekong7 <ikekong7@gmail.com>
2019-10-20 12:42:38 +0800

    binary search

--- binary_tree/BST.h
+++ binary_tree/BST.h
@@ -1,1 +1,69 @@
 #pragma once
+#include <iostream>
+
+using namespace std;
+
+template<typename Key, typename Value>
+class BST {
+private:
+	struct Node {
+		Key key;
+		Value value;
+		Node* left;
+		Node* right
+
+		Node(Key key, Value value) {
+			key = key;
+			value = value;
+			left = NULL;
+			right = NULL;
+		}
+	};
+
+	Node* root;
+	int count;
+
+public:
+	BST() {
+		root = NULL;
+		count = 0;
+	}
+
+	~BST() {
+
+	}
+
+	int size(){
+		return count;
+	}
+
+	int isEmpty() {
+		return count == NULL;
+	}
+
+	void insert(Key key, Value value) {
+		root = insert(root, key, value);
+	}
+
+private:
+	Node* insert(Node* node, Key key, Value value) {
+		
+		if (node == NULL) {
+			Node* tem = new Node(key, value);
+			node = tem;
+			count++;
+		}
+
+		if (node->key == key) {
+			node->value = value;
+		}
+		else if (node->key > key) {
+			node->left = insert(node->left, key, value);
+		}
+		else {
+			node->right = insert(node->right, key, value);
+		}
+
+		return node;
+	}
+};
